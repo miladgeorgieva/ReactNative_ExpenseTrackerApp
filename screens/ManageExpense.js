66a1dc59ee2d990a6999a7421241a09b3d8796df
milahristova <hristova.mila@abv.bg>
2022-07-27 09:35:52 +0300

    Adds loading state

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,12 +1,14 @@
-import { useContext, useLayoutEffect } from 'react';
+import { useContext, useLayoutEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import ExpenseForm from '../components/ManageExpense/ExpenseForm';
 import IconButton from '../components/UI/IconButton';
+import LoadingOverlay from '../components/UI/LoadingOverlay';
 import { GlobalStyles } from '../constants/styles';
 import { ExpensesContext } from '../store/expenses-context';
 import { deleteExpense, storeExpense, updateExpense } from '../util/http';
 
 function ManageExpenses({ route, navigation }) {
+   const [isSubmitting, setIsSubmitting] = useState(false);
     const expensesCtx = useContext(ExpensesContext);
 
     const editedExpenseId = route.params?.expenseId;
@@ -20,7 +22,9 @@ function ManageExpenses({ route, navigation }) {
     }, [navigation, isEditing]);
 
     async function deleteExpenseHandler() {
+        setIsSubmitting(true);
         await deleteExpense(editedExpenseId);
+        // setIsSubmitting(false);
         expensesCtx.deleteExpense(editedExpenseId);
         navigation.goBack();
     }
@@ -30,6 +34,8 @@ function ManageExpenses({ route, navigation }) {
     }
 
     async function confirmHandler(expenseData) {
+        setIsSubmitting(true);
+        
         if (isEditing) {
             expensesCtx.updateExpense(editedExpenseId, expenseData);
             await updateExpense(editedExpenseId, expenseData);
@@ -38,6 +44,10 @@ function ManageExpenses({ route, navigation }) {
             expensesCtx.addExpense({ ...expenseData, id: id });
         }
         navigation.goBack();
+    }
+
+    if (isSubmitting) {
+        return <LoadingOverlay />;
     }
 
     return (
--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,22 +1,31 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput';
+import LoadingOverlay from '../components/UI/LoadingOverlay';
 import { ExpensesContext } from '../store/expenses-context';
 import { getDateMinusDays } from '../util/date';
 import { fetchExpenses } from '../util/http';
 
 function RecentExpenses() {
+    const [isFetching, setIsFetching] = useState(true);
+
     const expensesCtx = useContext(ExpensesContext);
 
     useEffect(() => {
+        setIsFetching(true);
         /* Creating an async function inside so we dont add async on the useEffect hook 
         because it's not recommended by the React Native team */
         async function getExpenses() {
             const expenses = await fetchExpenses();
+            setIsFetching(false);
             expensesCtx.setExpenses(expenses);
         }
 
         getExpenses();
     }, []);
+
+    if (isFetching) {
+        return <LoadingOverlay />
+    }
 
     const recentExpenses = expensesCtx.expenses.filter((expense) => {
         const today = new Date();
